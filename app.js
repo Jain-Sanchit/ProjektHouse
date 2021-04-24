f3fdb947f3901e9bfbd22de138cbe605cb61d478
Jain-Sanchit <Jainsanchit157@gmail.com>
2021-04-24 13:56:06 +0530

    changed port after deployment error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const { MONGO_URI } = require("./config/keys");
 
 mongoose
